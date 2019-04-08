dcf1fe98e790c21aa022a4361d219bb86ad870ca
Aaron Lafferty <alafferty@valorem.com>
2019-04-08 16:10:05 -0500

    Added Powershell script for adding storage spaces

--- scripts/helloWorld.ps1
+++ scripts/helloWorld.ps1
@@ -1,2 +1,22 @@
-Write-Host "Hello World!"
-New-Item -Path "c:\" -Name "testfile1.txt" -ItemType "file" -Value "This is a text string."
+#Set Timezone to Central
+Set-TimeZone -Name "Central Standard Time"
+
+#Disable Windows Firewall
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+
+#Disable IE Enhanced Security Configuration
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+#Configure Storge Pool from attached disks
+Get-StoragePool -IsPrimordial $true | Get-PhysicalDisk | Where-Object CanPool -eq $True
+New-StoragePool –FriendlyName StoragePool1 –StorageSubsystemFriendlyName “Windows Storage*” –PhysicalDisks (Get-PhysicalDisk –CanPool $True)
+New-VirtualDisk –StoragePoolFriendlyName StoragePool1 –FriendlyName VirtualDisk1 –ResiliencySettingName Simple –UseMaximumSize
+Get-VirtualDisk –FriendlyName VirtualDisk1 | Get-Disk | Initialize-Disk –Passthru | New-Partition –AssignDriveLetter –UseMaximumSize | Format-Volume
